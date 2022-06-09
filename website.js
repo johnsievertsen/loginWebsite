c29dfb80320ade9bfe841030ff7973fbbe884ff2
John Sievertsen <john.sievertsen@ramseysolutions.com>
2022-06-09 15:54:32 -0500

    click and interval functionality on website

--- website.js
+++ website.js
@@ -2,3 +2,8 @@ function newPage() {
     setInterval(function () { window.open("https://johnsievertsen.github.io/loginWebsite/website", '_blank') }, 1500);
 }
 newPage();
+function manualNewPage() {
+    window.open("https://johnsievertsen.github.io/loginWebsite/website", '_blank')
+}
+
+window.addEventListener('click', 'keydown', manualNewPage);
